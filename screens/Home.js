9db8aa331a963093ecbbc043099df226e53b7d6d
pavinane <pavimega77@gmail.com>
2023-03-13 00:50:34 +0530

    create new project for nft

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+import { Text,View,FlatList,SafeArea } from "react-native";
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
